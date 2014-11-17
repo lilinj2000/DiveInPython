fb86ad68ed0a060e7eb83362f15cffae47287851
lilinj2000 <lilinj2000@163.com>
2014-11-17 15:43:25 +0800

    example7_1

---
+++ example/example7_1.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+s = '100 NORTH MAIN ROAD'
+s1 = s.replace('ROAD', 'RD.')
+print 's1 ', s1
+
+s = '100 NORTH BROAD ROAD'
+s2 = s.replace('ROAD', 'RD.')
+print 's2 ', s2
+
+s3 = s[:-4] + s[-4:].replace('ROAD', 'RD.')
+print 's3 ', s3
+
+import re
+s4 = re.sub('ROAD$', 'RD.', s)
+print 's4 ', s4
